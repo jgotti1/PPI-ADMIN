ceda043c513d0dedde5c6342290e97612640880a
jgotti1 <jgotti1@optonline.net>
2022-04-20 17:41:42 -0400

    java added

---
+++ java.js
@@ -0,0 +1,7 @@
+let link = document.querySelector(".dns-test");
+link.addEventListener("click", function () {
+  CallMe();
+});
+function CallMe() {
+  window.open("file://transfer.advancelocal.net");
+}
