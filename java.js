026203ad5a115641fac4b2d5fe911891ed63145e
jgotti1 <jgotti1@optonline.net>
2022-04-21 15:41:06 -0400

    finial version 2-21-22

--- java.js
+++
@@ -1,7 +0,0 @@
-let link = document.querySelector(".dns-test");
-link.addEventListener("click", function () {
-  CallMe();
-});
-function CallMe() {
-  window.open("file://transfer.advancelocal.net");
-}
